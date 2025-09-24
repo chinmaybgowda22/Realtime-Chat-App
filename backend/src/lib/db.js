b804cae0ed7ba30800c218541f3bcca9a78f725b
chinmaybgowda <chinmaybgowda789@gmail.com>
2025-09-24 12:08:18 +0530

    Initial commit - fullstack chat app

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,10 +1,13 @@
+
+
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log("MongoDB connection error:", error);
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // stop the server if DB fails
   }
 };
